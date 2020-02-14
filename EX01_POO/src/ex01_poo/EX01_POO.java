6e2cae4c9d4f86334f619ab21515f52c7aa59f41
brunoclaumari <brunoclaumari@gmail.com>
2020-02-14 03:55:47 -0300

    Fazendo ex01 modo console POO com classe

--- EX01_POO/src/ex01_poo/EX01_POO.java
+++ EX01_POO/src/ex01_poo/EX01_POO.java
@@ -5,6 +5,11 @@
  */
 package ex01_poo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  *
  * @author 082170034
@@ -14,8 +19,30 @@ public class EX01_POO {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParseException {
+        Jogo game = new Jogo();
+        Scanner lendo = new Scanner(System.in);
+        
+
+        System.out.println("Digite o código do jogo: ");
+        game.setCodigo(Integer.parseInt(lendo.next()));
+        lendo.nextLine();
+
+        System.out.println("Digite o nome do jogo: ");
+        game.setNome(lendo.nextLine());
+
+        System.out.println("Digite a categoria do jogo: ");
+        game.setCategoria(lendo.nextLine());        
+
+        System.out.println("Digite a data de lançamento do jogo: ");
+        
+        String dig = lendo.next();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date dt = formato.parse(dig);
+        game.setData(dt);
+
+        System.out.println(game.toString());
+
     }
-    
+
 }
---
+++ EX01_POO/src/ex01_poo/Jogo.java
@@ -0,0 +1,83 @@
+package ex01_poo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author BRUNOSILVA
+ */
+class Jogo {
+
+    private int codigo;
+    private String nome;
+    private String categoria;
+    private Date data;
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the data
+     */
+    public Date getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+     @Override
+    public String toString() {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        
+        return "Código: "+ this.codigo +
+                "\nNome: " + this.nome + 
+                "\nCategoria: " + this.categoria + 
+                "\nData de Lançamento: " + formato.format(this.data);
+    }
+
+}
